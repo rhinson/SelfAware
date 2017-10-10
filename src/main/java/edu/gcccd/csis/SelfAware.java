0a297ca783f46b7f1501427fa9f989e898acdbf2
rhinson <rogerhinson@gmail.com>
2017-10-09 20:22:38 -0700

    Changed search method

--- src/main/java/edu/gcccd/csis/SelfAware.java
+++ src/main/java/edu/gcccd/csis/SelfAware.java
@@ -17,8 +17,10 @@ public class SelfAware implements Language {
 
         SelfAware sa = new SelfAware();
 
+        // Uncomment to see results while testing without appending to file
 //        System.out.println("Keyword occurrences: " + sa.occurrences(code));
 
+        // Comment out to stop appending to file
         sa.append(code, "\n//Keyword occurrences: " + sa.occurrences(code));
 
     }
@@ -29,21 +31,45 @@ public class SelfAware implements Language {
         System.out.println(s);
 
         Language.sort(); // sort the array of Reserved Words
-        int n = 0; // occurrences counter
+        int n = 0; // total occurrences of keywords counter
+
+        String[] checkString = s.split("\\W+"); // Split string into array of words
 
         for (String eachWord: ReservedWords) {
-            int y = 0;
-            int k = -1; // progressed through k
-            while (-1 != (k = s.indexOf(eachWord, k + 1))) {
-                n++;
-                y++;
+            int y = 0; // track instances of each word
+            for (String myString : checkString) {
+                if (eachWord.equals(myString)) {
+//                    System.out.println(eachWord);
+                    n++;
+                    y++;
+                }
             }
-            // Uncomment to show number of each word found
+            // Uncomment to see count of each word found
 //            if ( y > 0 ) {
 //                System.out.println("Keyword: " + eachWord + ": " + y);
 //            }
+
         }
         return n;
+
+        // Original method of finding matches
+        // It found results within a word rather than the whole word
+//        for (String eachWord: ReservedWords) {
+//            int y = 0;
+//            int k = -1; // progressed through k
+//            while (-1 != (k = s.indexOf(eachWord, k + 1))) {
+//                System.out.println(eachWord);
+//                n++;
+//                y++;
+//            }
+//            // Uncomment to show number of each word found
+//            if ( y > 0 ) {
+//                System.out.println("Keyword: " + eachWord + ": " + y);
+//            }
+//        }
+//        return n;
+
+
     }
 
     public void append(String sourceFile, String message) throws IOException {
@@ -52,5 +78,6 @@ public class SelfAware implements Language {
     }
 }
 
+// I've added abstract to get an extra keyword
 //Keyword occurrences: 38
-//Keyword occurrences: 38
+//Keyword occurrences: 39
