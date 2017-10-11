92bfc28fa1e7d6330c7430baff81de73f967a4b7
rhinson <rogerhinson@gmail.com>
2017-10-10 23:07:33 -0700

    Cleared out some commented code Added in counter for number of checks performed

--- src/main/java/edu/gcccd/csis/SelfAware.java
+++ src/main/java/edu/gcccd/csis/SelfAware.java
@@ -17,9 +17,6 @@ public class SelfAware implements Language {
 
         SelfAware sa = new SelfAware();
 
-        // Uncomment to see results while testing without appending to file
-//        System.out.println("Keyword occurrences: " + sa.occurrences(code));
-
         // Comment out to stop appending to file
         sa.append(code, "\n//Keyword occurrences: " + sa.occurrences(code));
 
@@ -28,48 +25,26 @@ public class SelfAware implements Language {
     public int occurrences(String sourceFile) throws Exception {
 
         final String s = new String(Files.readAllBytes(Paths.get(sourceFile)));
-        System.out.println(s);
+        int n = 0; // total occurrences of keywords counter
+        int c = 0; // track total number of checks performed
 
         Language.sort(); // sort the array of Reserved Words
-        int n = 0; // total occurrences of keywords counter
 
         String[] checkString = s.split("\\W+"); // Split string into array of words
 
         for (String eachWord: ReservedWords) {
-            int y = 0; // track instances of each word
             for (String myString : checkString) {
+                c++;
                 if (eachWord.equals(myString)) {
-//                    System.out.println(eachWord);
+                    System.out.println(eachWord);
                     n++;
-                    y++;
                 }
             }
-            // Uncomment to see count of each word found
-//            if ( y > 0 ) {
-//                System.out.println("Keyword: " + eachWord + ": " + y);
-//            }
-
         }
+
+        System.out.println("Number of string comparisons: " + c);
+
         return n;
-
-        // Original method of finding matches
-        // It found results within a word rather than the whole word
-//        for (String eachWord: ReservedWords) {
-//            int y = 0;
-//            int k = -1; // progressed through k
-//            while (-1 != (k = s.indexOf(eachWord, k + 1))) {
-//                System.out.println(eachWord);
-//                n++;
-//                y++;
-//            }
-//            // Uncomment to show number of each word found
-//            if ( y > 0 ) {
-//                System.out.println("Keyword: " + eachWord + ": " + y);
-//            }
-//        }
-//        return n;
-
-
     }
 
     public void append(String sourceFile, String message) throws IOException {
